2752603f47dc759341e726089fcf3d06a1783b0b
Nishi <50321052+nishigthb@users.noreply.github.com>
2021-06-10 17:49:59 +0530

    folder added

---
+++ BlogApp/tut1.py
@@ -0,0 +1,13 @@
+from flask import Flask,render_template
+app=Flask(__name__)
+@app.route("/")
+def hello():
+    return render_template('index.html')
+@app.route("/about")
+def nishi():
+    name = "nishi"
+    clg = "KJSCE"
+    branch = "Computer"
+    year = "TY"
+    return render_template("about.html",name=name,clg=clg,branch=branch,year=year)
+app.run(debug=True)
